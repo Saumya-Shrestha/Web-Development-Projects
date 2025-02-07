a11d13465b42107e583edce9f59dc963d704029c
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-02-07 19:04:30 +0545

    Section 16 Completed

---
+++ Section 16-The Document Object Model (DOM)/DOM Challenge Starting Files/index.js
@@ -0,0 +1,9 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Saumya";
+
+document.querySelector(".list a").style.color = "red";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
+
+document.querySelector(".heading").classList.add("huge");
+
+document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
