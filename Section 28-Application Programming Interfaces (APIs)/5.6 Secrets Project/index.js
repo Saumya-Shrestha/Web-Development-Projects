a63c27ae368ae06a15cf8d755bba360a108a8988
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-02-17 19:27:16 +0545

    Secrets Project Completed

--- Section 28-Application Programming Interfaces (APIs)/5.6 Secrets Project/index.js
+++ Section 28-Application Programming Interfaces (APIs)/5.6 Secrets Project/index.js
@@ -11,3 +11,28 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "/random");
+    res.render("index.ejs", {
+      secret: response.data.secret,
+      user: response.data.username,
+    });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
